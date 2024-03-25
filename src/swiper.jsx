701f2223b8bfd7ecf1b44fd1e78e87f99acc4b35
Daniel Erenrich <daniel@erenrich.net>
2024-03-24 21:03:15 -0700

    fix arrow keys

--- src/swiper.jsx
+++ src/swiper.jsx
@@ -65,7 +65,7 @@ export function Swiper({ children, fetchMore, slugs }) {
         return () => {
             window.removeEventListener('keydown', handleKey);
         };
-    }, []);
+    }, [page, children]);
 
     return (
         <>
