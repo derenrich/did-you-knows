0d45096e15dbd1c1fd840c2a49701b8a74e95e52
Daniel Erenrich <daniel@erenrich.net>
2024-04-01 23:24:15 -0700

    oauth support

--- did_you_knows/__init__.py
+++ did_you_knows/__init__.py
@@ -2,13 +2,17 @@ from datetime import date
 from typing import Annotated
 from importlib import resources as impresources
 import pathlib
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Request, Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from sqlmodel import Session
 from . import database, crud
 from .database import engine
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.config import Config
+from starlette.datastructures import Secret
+from authlib.integrations.starlette_client import OAuth
 import httpx
 
 
@@ -26,7 +30,22 @@ async def get_http_client():
 
 HttpClient = Annotated[httpx.AsyncClient, Depends(get_http_client)]
 
+config = Config(env_prefix='DYK_')
+SESSION_SECRET = config('SESSION_SECRET', cast=Secret, default="DEFAULT_SECRET")
+
 app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key=SESSION_SECRET)
+
+oauth = OAuth(config)
+
+META_WIKI_OAUTH = "https://meta.wikimedia.org/w/index.php?title=Special:OAuth"
+
+oauth.register(
+    name='wikimedia',
+    request_token_url=f'{META_WIKI_OAUTH}/initiate',
+    access_token_url=f'{META_WIKI_OAUTH}/token',
+    authorize_url=f'{META_WIKI_OAUTH}/authenticate',
+)
 
 
 app.add_middleware(
@@ -35,6 +54,19 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.get("/login/wikimedia")
+async def login_wmf(request: Request):
+    redirect_uri = request.url_for('auth_via_wmf')
+    print(redirect_uri)
+    return await oauth.wikimedia.authorize_redirect(request, redirect_uri)
+
+
+@app.get("/login/auth_wikimedia")
+async def auth_via_wmf(request: Request):
+    token = await oauth.wikimedia.authorize_access_token(request)
+    return token
 
 
 @app.get("/api/hook/{hook_id}", response_model=database.Hook)
@@ -48,6 +80,24 @@ def read_hook(hook_id: int, session: DbSession):
 @app.get("/api/random_hooks/{number}", response_model=list[database.Hook])
 def read_hook(number: int, session: DbSession):
     return crud.get_random_hooks(session, number)
+
+
+@app.get("/api/suggested_hooks", response_model=list[database.Hook])
+def suggested_hooks(request: Request, session: DbSession):
+    user = request.session.get('user')
+    if not user:
+        return []
+    # TODO: return suggested hooks
+
+
+@app.get("/api/like_hook/{number}", response_model=bool)
+def like_hook(number: int, request: Request, session: DbSession):
+    user = request.session.get('user')
+    if not user:
+        return False
+    if user:
+        # TODO: write down the like
+        return True
 
 
 @app.post("/api/images", response_model=dict[int, str | None])
