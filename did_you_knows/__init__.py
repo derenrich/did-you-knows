edb1827c04412aef38ca6bf65c8f8ca8e84cc5bb
Daniel Erenrich <daniel@erenrich.net>
2024-04-07 22:55:43 -0700

    oauth get JWT

--- did_you_knows/__init__.py
+++ did_you_knows/__init__.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.config import Config
 from starlette.datastructures import Secret
 from authlib.integrations.starlette_client import OAuth
+from authlib.integrations.httpx_client import OAuth1Auth
 from .static import static_router, mount_static
 from .api import api_router
+from .dependencies import HttpClient
 
 config = Config(env_prefix="DYK_")
 SESSION_SECRET = config("SESSION_SECRET", cast=Secret)
@@ -46,9 +48,26 @@ async def login_wmf(request: Request):
 
 
 @app.get("/login/auth_wikimedia")
-async def auth_via_wmf(request: Request):
+async def auth_via_wmf(request: Request, client: HttpClient) -> bytes:
     token = await oauth.wikimedia.authorize_access_token(request)
-    return token
+
+    if token:
+        auth = OAuth1Auth(
+            client_id=config("CLIENT_ID"),
+            token=token.get("oauth_token"),
+            token_secret=token.get("oauth_token_secret"),
+        )
+
+        d = await client.get(
+            "https://en.wikipedia.org/wiki/Special:OAuth/identify", auth=auth
+        )
+        return d.content
+    raise HTTPException(401, "could not get access token")
+
+
+@app.get("/login/user")
+async def get_user():
+    pass
 
 
 app.include_router(api_router)
